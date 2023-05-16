eff1f6e00f0d735a1e0786656e4e7892ed9a4871
Vitoria Luz <110250731+Vitorialuz229@users.noreply.github.com>
2023-05-15 21:10:05 -0300

    Add files via upload

---
+++ T07/Pessoa.java
@@ -0,0 +1,13 @@
+package output.projetos.T07;
+
+public class Pessoa {
+    private short dia;
+    private short mes;
+    private short ano;
+    public short idade;
+}
+
+public class Pessoa {
+    private Data nascimento;
+    private String nome;
+}
---
+++ T07/Pessoa1.java
@@ -0,0 +1,6 @@
+package output.projetos.T07;
+
+public class Pessoa1 {
+    private short dia; 
+    private short
+}
