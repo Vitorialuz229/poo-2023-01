5f3b2448cd10dd35e5ba954bab56413052048152
vitorialuz229 <vitorialuz831@gmail.com>
2023-06-05 21:31:35 -0300

    t08

--- T08/src/main/java/com/github/vitorialuz229/poo/t08/Endereco.java
+++ T08/src/main/java/com/github/vitorialuz229/poo/t08/Endereco.java
@@ -3,5 +3,5 @@ package main.java.com.github.vitorialuz229.poo.t08;
 public class Endereco {
 }
 
-public class Escola extends Endereco {
+class Escola extends Endereco {
 }
--- T08/src/main/java/com/github/vitorialuz229/poo/t08/Escola.java
+++
@@ -1,3 +0,0 @@
-public class Escola {
-    private String Escola;
-}
