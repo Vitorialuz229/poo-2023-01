cf88d4675d445aa542a71167940d1d6868409a49
vitorialuz229 <vitorialuz831@gmail.com>
2023-06-05 21:49:53 -0300

    t08

--- T08/src/main/java/com/github/vitorialuz229/poo/t08/Linguagem.java
+++ T08/src/main/java/com/github/vitorialuz229/poo/t08/Linguagem.java
@@ -1,3 +1,7 @@
+package main.java.com.github.vitorialuz229.poo.t08;
+
+import src.main.java.com.github.vitorialuz229.poo.t08.Projeto;
+
 public class Linguagem {
     private Projeto[] projetos;
 }
--- T08/src/main/java/com/github/vitorialuz229/poo/t08/Projeto.java
+++ T08/src/main/java/com/github/vitorialuz229/poo/t08/Projeto.java
@@ -1,5 +1,7 @@
 package src.main.java.com.github.vitorialuz229.poo.t08;
 
+import main.java.com.github.vitorialuz229.poo.t08.Linguagem;
+
 public class Projeto {
     private Linguagem[] linguagem;
     private ArrayList pessoas;
