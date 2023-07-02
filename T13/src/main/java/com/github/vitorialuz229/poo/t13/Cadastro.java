2e43a44593a2da5ba97283f4073c7f15e7e6ef3e
vitorialuz229 <vitorialuz831@gmail.com>
2023-07-01 22:24:17 -0300

    inicial

---
+++ T13/src/main/java/com/github/vitorialuz229/poo/t13/Cadastro.java
@@ -0,0 +1,23 @@
+package com.github.vitorialuz229.poo.t13;
+
+import java.util.Scanner;
+
+public class Cadastro {
+
+    public static void main(String[]args) { 
+        String name, sobrenome; 
+        String email; 
+        String telefone; 
+        String cpf;
+
+        public Cadastro(String nome, String sobrenome, String email, String telefone, String cpf) {
+            this.nome = nome; 
+            this.sobrenome = sobrenome; 
+            this.email = email; 
+            this.telefone = telefone; 
+            this.cpf = cpf; 
+
+        
+        } 
+    }
+}
---
+++ T13/src/main/java/com/github/vitorialuz229/poo/t13/Hello.java
@@ -0,0 +1,7 @@
+package com.github.vitorialuz229.poo.t13;
+
+public class Hello {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
